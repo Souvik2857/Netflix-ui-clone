4a62fe849a5678a8531542d211f4ef4d8842b94e
Souvik Sarkar <sarkarsouvik2343@gmail.com>
2026-04-26 21:32:55 +0530

    enter keyboard key

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
-document.querySelectorAll('.red-btn').forEach((btn,index)=>{
-    btn.addEventListener('click',()=>{
-    const inputs=document.querySelectorAll('.input');
-    const data=inputs[index];
-
-    let value=data.value;
-    let gmail='@'+value.split('@')[1];
-    if(value===''){
+function submit(input) {
+    
+    let value = input.value;
+    let gmail = '@' + value.split('@')[1];
+    if (value === '') {
         alert('Enter a valid Email address');
-        data.value='';
+        input.value = '';
     }
-    else if(gmail==='@gmail.com'){
+    else if (gmail === '@gmail.com') {
         alert('Thanks for login');
-        data.value='';
+        input.value = '';
     }
-    else{
+    else {
         alert('Please Enter valid google email address');
-        data.value='';
+        input.value = '';
     }
-})
+}
+
+document.querySelectorAll('.red-btn').forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        const inputs = document.querySelectorAll('.input');
+        let data = inputs[index];
+        submit(data);
+    });
+});
+document.querySelectorAll('.input').forEach((input) => {
+    input.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            submit(input);
+        }
+    })
 })
